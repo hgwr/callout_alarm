5529d6aa6c8216ed817352c6296da833a8b144fd
Shigeru Hagiwara <hgwrsgr@gmail.com>
2018-02-25 10:34:22 +0900

    implementing controller

--- lib/alarm_controller.rb
+++ lib/alarm_controller.rb
@@ -1,11 +1,50 @@
 class AlarmController
-  attr_accessor :data
+  attr_accessor :data, :state
   
   def initialize(data)
     @data = data
+    @state = :inactive
+  end
+
+  def active?
+    state == :active
+  end
+
+  def inactive?
+    state == :inactive
   end
 
   def start
-    
+    state = :inactive
+    on_start
+
+    while true
+      if data.active_stage?
+        if inactive? 
+          state = :active
+          on_start_active
+        end
+        on_active
+      else
+        if !data.active_stage?
+          state = :inactive
+          on_start_inactive
+        end        
+      end
+
+      sleep 1
+    end
+  end
+
+  def on_start
+  end
+
+  def on_start_active
+  end
+
+  def on_active
+  end
+
+  def on_start_inactive
   end
 end
